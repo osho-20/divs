07463f3ff91d311eaa5f081e376b4c937aa06fd4
YASH JAIN <76126720+osho-20@users.noreply.github.com>
2021-10-18 23:44:52 +0530

    Create birthday.cpp

---
+++ birthday.cpp
@@ -0,0 +1,423 @@
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <thread>
+#include <unistd.h>
+using namespace std;
+int main()
+{
+    string Name;
+    system("cls");
+    cout << "                                                  EXCUSE ME!!!" << endl
+         << "                                           CAN I KNOW YOUR NAME???" << endl;
+    cout << "                                                    ";
+    cin >> Name;
+    sleep(1);
+    string s = "\t\t\t\t\t\tHELLO ";
+    for (int j = 0; j < s.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s[j];
+    }
+
+    for (int j = 0; j < Name.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << Name[j];
+    }
+
+    for (int j = 0; j < 3; j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << "!";
+    }
+
+    cout << endl;
+    string s2 = "\t\t\t    MY NAME IS OSHO AND I AM A BOT OPPOINTED FOR YOUR CONVENIENCE.";
+    for (int j = 0; j < s2.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s2[j];
+    }
+
+    cout << endl;
+    string s3 = "\t\t\t    I AM VERY HAPPY AFTER KNOWING THAT IT'S YOUR BIRTHDAY TODAY! :)";
+    for (int j = 0; j < s3.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s3[j];
+    }
+    cout << endl;
+start:
+
+    string s4 = "\t\t\t    SO WOULD YOU LIKE TO CELEBRATE YOUR BIRTHDAY WITH ME(Y/N)";
+    for (int j = 0; j < s4.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s4[j];
+    }
+
+    cout << endl;
+    char Y;
+
+    cout << "                                                    ";
+    cin >> Y;
+    string s5 = "\t\t\t     I AM HAPPY TO KNOW YOU WANT TO CELEBARTE IT WITH ME :)";
+    string s6 = "\t\t\t    YOU DONT HAVE ANY OTHER OPTION YOU HAVE TO CELEBRATE IT WITH ME :)";
+    string s7 = "\t\t\t    PLS TYPE CORRECT OPTIONS I MAY LOOK LIKE AN IDIOT BUT I'M NOT :)";
+    string s8 = "\t\t\t         SO LETS GET STARTED WITHOUT WASTING YOUR TIME :)";
+    string s9 = "\t\t\t    SO WE ARE STARTING YOUR BIRTHDAY WITH A BIRTHDAY WISH SONG :)";
+
+    if (Y == 'Y' || Y == 'y')
+    {
+        for (int j = 0; j < s5.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s5[j];
+        }
+        cout << endl;
+        for (int j = 0; j < s8.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s8[j];
+        }
+    }
+    else if (Y == 'N' || Y == 'n')
+    {
+        for (int j = 0; j < s6.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s6[j];
+        }
+
+        cout << endl;
+        goto start;
+    }
+    else
+    {
+        for (int j = 0; j < s7.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s7[j];
+        }
+
+        cout << endl;
+        goto start;
+    }
+    sleep(5);
+    system("cls");
+    for (int j = 0; j < s9.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s9[j];
+    }
+    string s10 = "\t\t\t              Happy Birthday to You, cha, cha, cha.";
+    string s11 = "\t\t\t              Happy Birthday to You, cha, cha, cha.";
+    string s12 = "\t\t\t                    Happy Birthday Dear ";
+    string s13 = "\t\t\t                      Happy Birthday to You.";
+    string s14 = "\t\t\t   THE CAKE IS NOT REAL BUT AT THIS MOMENT I CAN OFFER YOU THIS ONLY :'-)";
+    string s15 = "\t\t\t            I HOPE YOU LIKED THE TASTE OF MY VIRTUAL CAKE :)";
+    cout << endl;
+    sleep(2);
+    for (int j = 0; j < s10.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s10[j];
+    }
+    cout << endl;
+    sleep(1);
+    for (int j = 0; j < s11.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s11[j];
+    }
+    cout << endl;
+    sleep(1);
+    for (int j = 0; j < s12.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s12[j];
+    }
+    for (int j = 0; j < Name.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << Name[j];
+    }
+    cout << endl;
+    sleep(2);
+    for (int j = 0; j < s13.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s13[j];
+    }
+    cout << endl;
+    cout << " " << endl;
+    sleep(2);
+    cout << "\t\t\t                         |          |          |" << endl;
+    cout << "\t\t\t                       .....      .....      ....." << endl;
+    cout << "\t\t\t                       |///|      |///|      |///|" << endl;
+    cout << "\t\t\t                       |   |      |   |      |   |" << endl;
+    cout << "\t\t\t                       |///|      |///|      |///|" << endl;
+    cout << "\t\t\t                       |   |      |   |      |   |" << endl;
+    cout << "\t\t\t                ........................................." << endl;
+    cout << "\t\t\t                |//////////////// HAPPY ////////////////|" << endl;
+    cout << "\t\t\t                |////////////// BIRTHDAY ///////////////|" << endl;
+    cout << "\t\t\t          ......................................................" << endl;
+    cout << "\t\t\t          |/////// MAY THIS YEAR BRING LOTS OF HAPPINESS //////|" << endl;
+    cout << "\t\t\t          |///////// FOR YOU AND YOU MAY GET EVERYTHING ///////|" << endl;
+    cout << "\t\t\t          |///////////// THAT YOU WANT IN YOUR LIFE. //////////|" << endl;
+    cout << "\t\t\t          ......................................................" << endl;
+    cout << "\t\t\t          |....................................................|" << endl;
+    sleep(10);
+    system("cls");
+    for (int j = 0; j < s14.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s14[j];
+    }
+    cout << endl;
+    for (int j = 0; j < s15.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s15[j];
+    }
+    cout << endl;
+    sleep(5);
+    system("cls");
+    string s16 = "\t\t\t               I HAVE TO DO ONLY THIS MUCH FOR YOU ";
+    string s17 = "\t\t\t          BUT AS YOU ARE SPECIAL IM AM EXTENDING MY LIMITS :) ";
+    string s18 = "\t\t\t       AND DONT TELL THIS TO YASH OTHERWISE HE IS GONNA KILL ME ;) ";
+    for (int j = 0; j < s16.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s16[j];
+    }
+    cout << endl;
+    for (int j = 0; j < s17.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s17[j];
+    }
+    cout << endl;
+    for (int j = 0; j < s18.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s18[j];
+    }
+    cout << endl;
+    string s19 = "\t\t\t                    SO WOULD YOU LIKE TO COUNTINUE(Y/N)";
+    string s20 = "\t\t\t             I HOPE YOU LIKED MY PROGRAM AND HOPE YOU ENJOYED IT :)";
+    string s21 = "\t\t\t                   BYE!! AND ONCE AGAIN A VERY HAPPY BIRTHDAY :)";
+    string s22 = "\t\t\t   SO YOU ARE IN A MOOD TO CHILL RIGHT NOW SO I HAVE SOME SHAYARIES FOR YOU :)";
+    string s23 = "Har pal ne kaha ek pal se... Pal bhar ke liye aap mere samne aa jao...";
+    string s24 = "\t\t\t            pal bhar ka saath kuch aisa ho...ki har pal tum hi yaad aao...";
+    string s25 = "Phool bankar muskarana zindagi,muskarake gum bhulana zindagi";
+    string s26 = "\t\t\t       jeet kar koi khush ho to kya hua,haar kar khushiya manana bhi zindagi...";
+    string s27 = "\t\t\t      1.";
+    string s28 = "\t\t\t      2.";
+    string s29 = "\t\t\t              AS YOU ARE THE BIRTHDAY GIRL I CANT LEAVE YOU LIKE THIS ;)";
+    string s30 = "\t\t\t       YOU HAVE TO SAY 'BYE' or 'bye' FIRST OTHERWISE I M NOT GONNA LEAVE ;)";
+    string s31 = "\t\t\t       SO NOW YOU CAN RETURN TO YASH HE IS HAVING SOME MORE SURPRISES FOR YOU ;)";
+    string s32 = "BYE";
+    string s33 = "bye";
+    string s34 = "\t\t\t                LOL!!! YOU DONT WANNA LEAVE ME OR YOU HAVE MISS TYPED";
+
+    for (int j = 0; j < s19.size(); j++)
+    {
+        for (int i = 0; i <= 59999999; i++)
+            ;
+        cout << s19[j];
+    }
+    cout << endl;
+    cout << "                                                           ";
+    cin >> Y;
+    if (Y == 'Y' || Y == 'y')
+    {
+        for (int j = 0; j < s22.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s22[j];
+        }
+        cout << endl;
+        sleep(5);
+        for (int j = 0; j < s27.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s27[j];
+        }
+        for (int j = 0; j < s23.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s23[j];
+        }
+
+        cout << endl;
+        for (int j = 0; j < s24.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s24[j];
+        }
+        sleep(5);
+        cout << endl;
+        for (int j = 0; j < s28.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s28[j];
+        }
+        for (int j = 0; j < s25.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s25[j];
+        }
+        cout << endl;
+        for (int j = 0; j < s26.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s26[j];
+        }
+        sleep(5);
+        cout << endl;
+
+    start1:
+        for (int j = 0; j < s29.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s29[j];
+        }
+        cout << endl;
+        for (int j = 0; j < s30.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s30[j];
+        }
+        cout << endl;
+        string b;
+        cout << "                                                           ";
+        cin >> b;
+        if (b == s32 || b == s33)
+        {
+
+            for (int j = 0; j < s31.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s31[j];
+            }
+            cout << endl;
+            for (int j = 0; j < s20.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s20[j];
+            }
+            cout << endl;
+            for (int j = 0; j < s21.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s21[j];
+            }
+            cout << endl;
+            sleep(5);
+        }
+        else
+        {
+            for (int j = 0; j < s34.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s34[j];
+            }
+            cout << endl;
+            goto start1;
+        }
+    }
+    else
+    {
+    start2:
+        for (int j = 0; j < s29.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s29[j];
+        }
+        cout << endl;
+        for (int j = 0; j < s30.size(); j++)
+        {
+            for (int i = 0; i <= 59999999; i++)
+                ;
+            cout << s30[j];
+        }
+        cout << endl;
+        string c;
+        cout << "                                                           ";
+        cin >> c;
+        if (c == s32 || c == s33)
+        {
+            for (int j = 0; j < s31.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s31[j];
+            }
+            cout << endl;
+            for (int j = 0; j < s20.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s20[j];
+            }
+            cout << endl;
+            for (int j = 0; j < s21.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s21[j];
+            }
+            cout << endl;
+            sleep(6);
+        }
+        else
+        {
+            for (int j = 0; j < s34.size(); j++)
+            {
+                for (int i = 0; i <= 59999999; i++)
+                    ;
+                cout << s34[j];
+            }
+            cout << endl;
+            goto start2;
+        }
+    }
+    return 0;
+}
